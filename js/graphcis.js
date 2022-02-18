424955da892d044706658a457906d50959da39b7
Mauricio Landeros <jairo.landeros2885@alumnos.udg.mx>
2022-02-18 12:15:26 -0600

    Particle system gravity

--- js/graphcis.js
+++ js/graphcis.js
@@ -13,17 +13,226 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
 const particleCount = 5000;
+var last_time_activate = Date.now();
 
-/* Torus particle */
+
+/* TORUS PARTICLES */
 const geometry = new THREE.TorusGeometry(10, 3, 20, 100);
 const material = new THREE.PointsMaterial({ 
     size: 0.005});
 const torus  =new THREE.Points(geometry, material);
 scene.add(torus);
-/***********/
 
-/* Particles */
-const particles = new THREE.BufferGeometry();
+
+/* PARTICLES */
+var MathUtils = {
+    getRandomInt: function(min, max)
+    {
+        return Math.floor(Math.random() * (max - min)) + min;
+    },
+    getDegree: function(radian)
+    {
+        return radian / Math.PI * 180;
+    },
+    getRadian: function(degrees)
+    {
+        return degrees * Math.PI / 180;
+    },
+    getSpherical: function(rad1, rad2, r)
+    {
+        var x = Math.cos(rad1) * Math.cos(rad2) * r;
+        var y = Math.sin(rad1) * r;
+        var z = Math.cos(rad1) * Math.sin(rad2) * r;
+
+        return [x, y, z];
+    }
+};
+
+class Mover {
+    constructor() {
+        this.position = new THREE.Vector3();
+        this.velocity = new THREE.Vector3();
+        this.acceleration = new THREE.Vector3();
+        this.anchor = new THREE.Vector3();
+        this.mass = 1;
+        this.r = 0;
+        this.g = 0;
+        this.b = 0;
+        this.a = 1;
+        this.time = 0;
+        this.isActive = false;
+    }
+    init(vector) {
+        this.position = vector.clone();
+        this.velocity = vector.clone();
+        this.anchor = vector.clone();
+        this.acceleration.set(0, 0, 0);
+        this.a = 1;
+        this.time = 0;
+    }
+    updatePosition() {
+        this.position.copy(this.velocity);
+    }
+    updateVelocity() {
+        this.acceleration.divideScalar(this.mass);
+        this.velocity.add(this.acceleration);
+    }
+    applyForce(vector) {
+        this.acceleration.add(vector);
+    }
+    /*applyFriction() {
+        var friction = Force.friction(this.acceleration, 0.1);
+        this.applyForce(friction);
+    }
+    applyDragForce(value) {
+        var drag = Force.drag(this.acceleration, value);
+        this.applyForce(drag);
+    }
+    hook(restLength, k) {
+        var force = Force.hook(this.velocity, this.anchor, restLength, k);
+        this.applyForce(force);
+    }*/
+    activate() {
+        this.isActive = true;
+    }
+    inactivate() {
+        this.isActive = false;
+    }
+};
+
+var moversCount = 5000;
+var movers = [];
+var pointsGeometry = null;
+var pointsMaterial = null;
+var pointsGeometry2 = null;
+var pointsMaterial2 = null;
+var points = null;
+var points2  =null;
+
+var drawCount = 25000;
+
+var antiGravity = new THREE.Vector3(0, 5, 0);
+
+var activateMover = function()
+{
+    var count = 0;
+    for(var i = 0; i < movers.length; i++)
+    {
+        var mover = movers[i];
+        if(mover.isActive) continue;
+        mover.activate();
+        mover.velocity.y = -300;
+        count++;
+        if(count >= 90) break;
+    }
+};
+
+var buildPoints = function()
+{
+    pointsGeometry = new THREE.BufferGeometry();
+    pointsMaterial = new THREE.PointsMaterial({
+        color: 0x77ffaa,
+        size: 0.01,
+        //tranparent: true,
+        //opacity: 0.5,
+        depthTest: false,
+        blending: THREE.AdditiveBlending
+    });
+
+    pointsGeometry2 = new THREE.BufferGeometry();
+    pointsMaterial2 = new THREE.PointsMaterial({
+        color: 0x77ffaa,
+        size: 0.01,
+        //tranparent: true,
+        //opacity: 0.5,
+        depthTest: false,
+        blending: THREE.AdditiveBlending
+    });
+    var vertices = [];
+    var vertices2 = [];
+    for(var i = 0; i < moversCount; i++)
+    {
+        var mover = new Mover();
+        var range = (1 - Math.log(MathUtils.getRandomInt(2, 256)) / Math.log(256)) * 500;
+        var rad = MathUtils.getRadian(MathUtils.getRandomInt(0, 360));
+        var x = Math.cos(rad) * range;
+        var z = Math.sin(rad) * range;
+        mover.init(new THREE.Vector3(x, 1000, z));
+        mover.mass = MathUtils.getRandomInt(300, 500) / 100;
+        movers.push(mover);
+
+        if(i % 2 === 0)
+        {
+            vertices.push(mover.position.x, mover.position.y, mover.position.z);
+            //pointsGeometry.vertices.push(mover.positon);
+        }
+        else
+        {
+            vertices2.push(mover.position.x, mover.position.y, mover.position.z);
+            //pointsGeometry2.vertices.push(mover.positon);
+        }
+    }
+
+    pointsGeometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+    pointsGeometry.setDrawRange(0, drawCount);
+    pointsGeometry.computeBoundingSphere();
+    pointsGeometry2.setAttribute('position', new THREE.Float32BufferAttribute(vertices2, 3));
+    pointsGeometry2.setDrawRange(0, drawCount);
+    pointsGeometry2.computeBoundingSphere();
+    
+    points = new THREE.Points(pointsGeometry, pointsMaterial);
+    points2 = new THREE.Points(pointsGeometry2, pointsMaterial2);
+    scene.add(points);
+    scene.add(points2);
+};
+
+var updatePoints = function()
+{
+    var pointsVertices = [];
+    var pointsVertices2 = [];
+
+    for(var i = 0; i < movers.length; i++)
+    {
+        var mover = movers[i];
+        if(mover.isActive)
+        {
+            mover.applyForce(antiGravity);
+            mover.updateVelocity();
+            mover.updatePosition();
+
+            if(mover.position.y > 500)
+            {
+                var range = (1 - Math.log(MathUtils.getRandomInt(2, 256)) / Math.log(256)) * 500;
+                var rad = MathUtils.getRadian(MathUtils.getRandomInt(0, 360));
+                var x = Math.cos(rad) * range;
+                var z = Math.sin(rad) * range;
+                mover.init(new THREE.Vector3(x, -300, z));
+                mover.mass = MathUtils.getRandomInt(300, 500) / 100;
+            }
+        }
+        if(i % 2 === 0)
+        {
+            pointsVertices.push(mover.position.x, mover.position.y, mover.position.z);
+        }
+        else
+        {
+            pointsVertices2.push(mover.position.x, mover.position.y, mover.position.z);
+        }
+    }
+    //points.geometry.vertices = pointsVertices;
+    pointsGeometry.setAttribute('position', new THREE.Float32BufferAttribute(pointsVertices, 3));
+    points.geometry.attributes.position.needsUpdate = true;
+    points.geometry.computeBoundingBox();
+    points.geometry.computeBoundingSphere();
+
+    //points2.geometry.vertices = pointsVertices2;
+    pointsGeometry.setAttribute('position', new THREE.Float32BufferAttribute(pointsVertices2, 3));
+    points2.geometry.attributes.position.needsUpdate = true;
+    points2.geometry.computeBoundingBox();
+    points2.geometry.computeBoundingSphere();
+};
+
+/*const particles = new THREE.BufferGeometry();
 const particlesMaterial = new THREE.PointsMaterial({ 
     size: 0.1,
     vertexColors: true});
@@ -55,7 +264,7 @@ particles.setAttribute('color', new THREE.Float32BufferAttribute(colorsArray, 3)
 particles.computeBoundingSphere();
 
 const particlesMesh = new THREE.Points(particles, particlesMaterial);
-scene.add(particlesMesh);
+scene.add(particlesMesh);*/
 
 
 /* LIGHT */
@@ -66,8 +275,27 @@ const pointLight = new THREE.PointLight(0xffffff, 0.8);
 pointLight.position.set(25, 50, 25);
 scene.add(pointLight);
 
-window.addEventListener('resize', onWindowResize);
+const PointLightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(gridHelper, PointLightHelper);
 
+const controls = new OrbitControls(camera, renderer.domElement);
+
+
+
+/* MOUSE CONTROLS */
+document.addEventListener('mousemove', animateParticles);
+var mouseX = 0;
+var mouseY = 0;
+
+function animateParticles(event)
+{
+    mouseX = event.clientX;
+    mouseY = event.clientY;
+}
+
+
+window.addEventListener('resize', onWindowResize);
 function onWindowResize()
 {
     camera.aspect = window.innerWidth / window.innerHeight;
@@ -80,11 +308,27 @@ const clock = new THREE.Clock();
 function tick()
 {
     const elapsedTime = clock.getElapsedTime();
+    var now = Date.now();
+    var bgTime = elapsedTime;
 
     requestAnimationFrame(tick);
+
+    if (now - last_time_activate > 10) {
+        activateMover();
+        last_time_activate = Date.now();
+    }
+
+    //particlesMesh.rotation.y = -0.1*elapsedTime;
+    updatePoints();
     torus.rotation.x = 0.5 * elapsedTime;
     torus.rotation.y = 0.5 * elapsedTime;
+    /*if(mouseX > 0)
+    {
+        particlesMesh.rotation.x = -mouseY * (bgTime * 0.00008);
+        particlesMesh.rotation.y = mouseX * (bgTime * 0.00008);
+    }*/
+    controls.update();
     renderer.render(scene, camera);
 }
-
+buildPoints();
 tick();
